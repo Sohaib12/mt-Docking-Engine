7d6565d8d7692c2890b2d2d685dcd240f9fdf367
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-23 18:47:32 +0200

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+if [ $ENV_TO_INJECT = "c4.large" ]; then
+  echo "It has 2 cores"
+elif [ $ENV_TO_INJECT = "c4.xlarge" ]; then
+  echo "It has 4 cores"
+elif [ $ENV_TO_INJECT = "c4.2xlarge" ]; then
+  echo "It has 8 cores"
+elif [ $ENV_TO_INJECT = "c4.4xlarge" ]; then
+  echo "It has 16 cores"
+elif [ $ENV_TO_INJECT = "c4.8xlarge" ]; then
+  echo "It has 36 cores"
+elif [ $ENV_TO_INJECT = "c3.large" ]; then
+  echo "It has 2 cores"
+elif [ $ENV_TO_INJECT = "c3.xlarge" ]; then
+  echo "It has 4 cores"
+elif [ $ENV_TO_INJECT = "c3.2xlarge" ]; then
+  echo "It has 8 cores"
+elif [ $ENV_TO_INJECT = "c3.4xlarge" ]; then
+  echo "It has 16 cores"
+elif [ $ENV_TO_INJECT = "c3.8xlarge" ]; then
+  echo "It has 32 cores"
+fi
