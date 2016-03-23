76458fff990eda0821085256a0feae62ef481baf
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-23 17:09:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+#mkdir go
+#export GOPATH="/go"
+#export PATH=$PATH:$GOPATH/bin
+
+cd x86_64Linux2
+./autogrid4
+
+cd ..
+
+#go get -u github.com/ddollar/forego
+foreman check
+foreman start
