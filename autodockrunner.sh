cefd230f9783dd802754bb3b7d758fb78a1bf8c8
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-24 19:27:20 +0200

    Update autodockrunner.sh

--- autodockrunner.sh
+++ autodockrunner.sh
@@ -1,4 +1,26 @@
 # !/bin/bash
+
+# This file should be dynamically edited.
+# For example it should take the names of the files (target, ligand) and then create (gpf, dpf) files from them.
+# Then it should run autogrid with the gpf file in arguments.
+# And then it should run autodock with the dpf file in arguments.
+
+# Recieve files/file names.
+
+
+# Create gpf files from target files.
+
+
+# Create dpf files from ligand files.
+
+
+# Run autogrid using gpd file as argument.
+
+
+# Run autodock using dpf file as argument.
+
+
+
 echo "autodocker runner starts!!!!!"
 
 
