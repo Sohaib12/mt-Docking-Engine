0b7b1421c2ecc23ce7f928c06e2ee767e2100d90
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-24 14:05:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,25 @@
 # !/bin/bash
 
-# Fire up another bash script that checks the type of...
+# Make the following bash scripts executable.
 chmod +x /table.sh
+chmod +x /autodockrunner.sh
+
+# Fire up another bash script that checks the capacity...
 bash table.sh "$ENV_TO_INJECT"
 
 # İnstall the necessary packages for running autogrid and autodock.
 cd yavst
 cd requirements
 pip install -r requirements.txt
+cd ..
+cd ..
 
-cd ..
-cd ..
+# Start foreman to run autodockrunner. This will fire up the Procfile that will give the available cores their tasks.
+foreman check
+foreman start
+
+# Fire up another bash script that runs the autodock depending on the capacity...
+
 
 # Start autogrid.
 cd x86_64Linux2
@@ -22,6 +31,3 @@ cd x86_64Linux2
 # Go to previous directory.
 cd ..
 
-# Start foreman to run autodockrunner. This will fire up the Procfile that will give the available cores their tasks.
-foreman check
-foreman start
