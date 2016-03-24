13ebd73e987113cf12d0c02394aa111f1de4a453
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-24 15:45:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ chmod +x /autodockrunner.sh
 # Check capacity and then dynamically create a Procfile that utilizes all the capacity.
 # Also it will start foreman to run autodockrunner. 
 # And will fire up the Procfile that will give the available cores their tasks.
-echo "this should be strange" > Procfile
+echo "this should be strange" > Procfile.txt
 
-if [ $ENV_TO_INJECT = "c4.large" ]; then
+if [ "$ENV_TO_INJECT" = "c4.large" ]; then
   echo "It has 2 cores"
   for i in {1..2}
     do 
@@ -16,39 +16,39 @@ if [ $ENV_TO_INJECT = "c4.large" ]; then
     done
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c4.xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c4.xlarge" ]; then
   echo "It has 4 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c4.2xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c4.2xlarge" ]; then
   echo "It has 8 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c4.4xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c4.4xlarge" ]; then
   echo "It has 16 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c4.8xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c4.8xlarge" ]; then
   echo "It has 36 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c3.large" ]; then
+elif [ "$ENV_TO_INJECT" = "c3.large" ]; then
   echo "It has 2 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c3.xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c3.xlarge" ]; then
   echo "It has 4 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c3.2xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c3.2xlarge" ]; then
   echo "It has 8 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c3.4xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c3.4xlarge" ]; then
   echo "It has 16 cores"
   foreman check
   foreman start
-elif [ $ENV_TO_INJECT = "c3.8xlarge" ]; then
+elif [ "$ENV_TO_INJECT" = "c3.8xlarge" ]; then
   echo "It has 32 cores"
   foreman check
   foreman start
