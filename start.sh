8afe1a66802407088c0deca31d81f603c7f913bc
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-24 15:16:47 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,6 +19,10 @@ chmod +x /autodockrunner.sh
 # And will fire up the Procfile that will give the available cores their tasks.
 if [ $ENV_TO_INJECT = "c4.large" ]; then
   echo "It has 2 cores"
+  for i in 1 2
+    do 
+      echo "it should run two times"
+    done
   foreman check
   foreman start
 elif [ $ENV_TO_INJECT = "c4.xlarge" ]; then
