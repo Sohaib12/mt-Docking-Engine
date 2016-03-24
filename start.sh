67afb3112d39c27a6c4938893b38aa8a8eb01cb3
Sohaib12 <mohammadsohaib@polizom.com>
2016-03-24 16:12:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,9 @@ chmod +x /autodockrunner.sh
 # Check capacity and then dynamically create a Procfile that utilizes all the capacity.
 # Also it will start foreman to run autodockrunner. 
 # And will fire up the Procfile that will give the available cores their tasks.
-#touch Procfile
-#echo "this should be strange" > Procfile
-
 if [ "$ENV_TO_INJECT" = "c4.large" ]; then
   echo "It has 2 cores"
-  for i in {1..4}
+  for i in {1..2}
     do 
       touch Procfile
       echo "worker: " >> Procfile
@@ -20,38 +17,83 @@ if [ "$ENV_TO_INJECT" = "c4.large" ]; then
   foreman start
 elif [ "$ENV_TO_INJECT" = "c4.xlarge" ]; then
   echo "It has 4 cores"
+  for i in {1..4}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c4.2xlarge" ]; then
   echo "It has 8 cores"
+  for i in {1..8}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c4.4xlarge" ]; then
   echo "It has 16 cores"
+  for i in {1..16}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c4.8xlarge" ]; then
   echo "It has 36 cores"
+  for i in {1..36}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c3.large" ]; then
   echo "It has 2 cores"
+  for i in {1..2}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c3.xlarge" ]; then
   echo "It has 4 cores"
+  for i in {1..4}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c3.2xlarge" ]; then
   echo "It has 8 cores"
+  for i in {1..8}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c3.4xlarge" ]; then
   echo "It has 16 cores"
+  for i in {1..16}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 elif [ "$ENV_TO_INJECT" = "c3.8xlarge" ]; then
   echo "It has 32 cores"
+  for i in {1..32}
+    do 
+      touch Procfile
+      echo "worker: " >> Procfile
+    done
   foreman check
   foreman start
 fi
