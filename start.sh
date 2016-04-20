6f16b19aa0116a6f4273ca4073c74e5183d663ef
Sohaib12 <mohammadsohaib@polizom.com>
2016-04-20 12:07:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,90 +1,10 @@
 #!/bin/bash
 
-# Make the following bash scripts executable.
-chmod +x /autodockrunner.sh
+# Run the java code..
 
-# Check capacity and then dynamically create a Procfile that utilizes all the capacity.
-# The Procfile will start  autodockrunner.sh in a parallel form.
-if [ "$ENV_TO_INJECT" = "c4.large" ]; then
-  echo "It has 2 cores"
-  for i in {1..2}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-    
-elif [ "$ENV_TO_INJECT" = "c4.xlarge" ]; then
-  echo "It has 4 cores"
-  for i in {1..4}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
 
-elif [ "$ENV_TO_INJECT" = "c4.2xlarge" ]; then
-  echo "It has 8 cores"
-  for i in {1..8}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
 
-elif [ "$ENV_TO_INJECT" = "c4.4xlarge" ]; then
-  echo "It has 16 cores"
-  for i in {1..16}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
+# Listen for an exit command.
 
-elif [ "$ENV_TO_INJECT" = "c4.8xlarge" ]; then
-  echo "It has 36 cores"
-  for i in {1..36}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-
-elif [ "$ENV_TO_INJECT" = "c3.large" ]; then
-  echo "It has 2 cores"
-  for i in {1..2}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-
-elif [ "$ENV_TO_INJECT" = "c3.xlarge" ]; then
-  echo "It has 4 cores"
-  for i in {1..4}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-
-elif [ "$ENV_TO_INJECT" = "c3.2xlarge" ]; then
-  echo "It has 8 cores"
-  for i in {1..8}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-
-elif [ "$ENV_TO_INJECT" = "c3.4xlarge" ]; then
-  echo "It has 16 cores"
-  for i in {1..16}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-    
-elif [ "$ENV_TO_INJECT" = "c3.8xlarge" ]; then
-  echo "It has 32 cores"
-  for i in {1..32}
-    do 
-      touch Procfile
-      echo "worker: ./autodockrunner.sh" >> Procfile
-    done
-fi
-
-foreman check
-foreman start
+trap 'exit 0' SIGTERM
+while true; do :; done
